ce349199570655c9a0bc8de205ae379ee0e82a9c
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-03-08 09:29:17 +0100

    postavljeni zadaci na 000

--- 000.php
+++ 000.php
@@ -1,74 +1,131 @@
-<?php
-declare(strict_types=1);
- ?>
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testiranje</title>
+</head>
+<body>
+    <!-- ZADACI -->
+    <fieldset><legend>Zadatak 1</legend>
+    <?php
+    // 1. Podesiti naslov dokumenta. Naslov je “Testiranje”.
+    //      ● Pod naslovom dokumenta misli se na <title> tag.
+    //      ● Koristiti echo instrukciju.
+    
 
-//  $var = 1;
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 2</legend>
+    <?php
+    // 2. Ispisati <h1> element na stranici, sa sledećim sadržajem: “Testiranje je počelo.”
+    //      ● Koristiti echo instrukciju.
+    
 
-//  function test () {
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 3</legend>
+    <?php
+    // 3. Napiši PHP program koji zamenjuje vrednosti dve promenljive.
+    //      ● x = 5
+    //      ● y = 10
+    
 
-//     global $svasta;
-//     echo $svasta;
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 4</legend>
+    <?php
+    // 4. Napiši PHP program za izračunavanje sume dve celobrojne vrednosti. Ukoliko su dve vrednosti iste, program vraća njihov trostruki zbir.
+    
 
-//         static $var = 3;
-//         $var = 2;
-//  }
-//  test();
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 5</legend>
+    <?php
+    // 5. Napiši PHP program koji ispisuje na stranici true, ukoliko je jedna od dve celobrojne vrednosti broj 30, ili ako im je suma 30 u suprotnom program ispisuje false.
+    //      ● ulaz: 30, 0
+    //       ○ izlaz: true
+    //      ● ulaz: 25, 5
+    //        ○ izlaz: true
+    //      ● ulaz: 20, 30
+    //        ○ izlaz: true
+    //      ● ulaz: 20, 25
+    //        ○ izlaz: false
+    
 
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 6</legend>
+    <?php
+    // 6. Napisati PHP program koji će proveriti da li je jedna od dve celobrojne vrednosti manja od 0, a druga veća od 100.
+    
 
-// define("AUDI", "q7");
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 7</legend>
+    <?php
+    // 7. Napisati PHP program koji će proveriti da li je jedna od dve celobrojne vrednosti u opsegu između 100 i 200. (ukljucujuci 100 i 200) Ukoliko jeste program vraca true, a ukoliko nije program vraca false.
+    
 
-// define("cars", [
-// "alfa",
-// "BMW",
-// "Toyota"
-// ]);
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 8</legend>
+    <?php
+    // 8. Napisati PHP program koji određuje najveci broj između tri celobrojne vrednosti i ispisati ga na stranicu.
+    //      ● Nije dozvoljeno korišćenje bilo koje ugrađjene funkcije za određivanje najvećeg broja.
+    
 
-// echo "test";
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 9</legend>
+    <?php
+    // 9. Napisati PHP program koji će proveriti koja od dve celobrojne vrednosti je bliža broju 100 i ispisati je. Ukoliko su brojevi jednaki, ispisati 0.
+    
 
-// $var
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 10</legend>
+    <?php
+    // 10. Napisati PHP program koji će ispisati na strinici true ukoliko su obe celobrojne vrednosti u opsegu izmedju 40 i 50 (ukljucujuci 40 i 50), ili su obe vrednosti u opsegu između 50 i 60 (ukljucujuci 50 i 60), u suprotnom false.
+    
 
-// $var1 = 1;
-// $var2 = 2;
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 11</legend>
+    <?php
+    // 
+    
 
-// echo $var1.$var2;
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 12</legend>
+    <?php
+    // 
+    
 
-// $var = "blue";
+    
+    ?>
+    </fieldset>
+    <fieldset><legend>Zadatak 13</legend>
+    <?php
+    // 
+    
 
-// switch ($var) {
-//     case "red":
-//         echo "Red...";
-//         break;
-//     case "blue":
-//         echo "Blue...";
-//         break;
-//     case "orange":
-//         echo "Orange...";
-//         break;
-//     case "green":
-//         echo "Green...";
-//         break;
-//     default:
-//         echo "dont know";
-// }
-
-// $arr = [1,2,3,4,5]; // indexed
-// $arr3 = ["Audi" => 3, "bmw"=>5]; // asocijativni niz
-// $arr2 = array(
-//     array("bmw","audi"),
-//     array("bmw","audi"),
-//     array("bmw","audi"),
-// );
-
-// for ($i = 0; $i<6; i++;) {
-//     if (i == 3) {
-//         continue;
-//     };
-//     echo $i;
-// }
-
-
-
-
-
-?>
+    
+    ?>
+    </fieldset>
+    <!-- ZADACI END -->
+</body>
+</html>
